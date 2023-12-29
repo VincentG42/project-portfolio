2a2115bc99b150750a322210220fc18cf01d0ada
VincentG42 <goyetvincent@gmail.com>
2023-12-29 22:52:31 +0100

    nav responsive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 
+//  nav bar hover
+const navlinks= document.querySelectorAll('.navlink')
+console.log(navlinks);
+navlinks.forEach((navLink)=>{
+    console.log(navLink);
+// let navLinkHover = gsap.to(navLink, {
+//     paused: true,
+//     opacity: 0
+//   });
+  
+//   navLink.addEventListener("mouseenter", () => navLinkHover.play());
+//   navLink.addEventListener("mouseleave", () => navLinkHover.reverse());
+})
+//  nav bar hover
 
 //  dark mode switch
 const darkMode = document.querySelector('#dark_mode');
@@ -23,7 +37,6 @@ darkMode.addEventListener('click', ()=>{
             document.documentElement.setAttribute('data-theme', 'light');
     }
 })
-
 
 
 //dark mode switch
