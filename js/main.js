e77f656d378b0251c6dc7b53f68a59fb806157e1
VincentG42 <goyetvincent@gmail.com>
2024-01-04 23:23:41 +0100

    debut nouvelle animation sections

--- js/main.js
+++ js/main.js
@@ -24,14 +24,40 @@ darkMode.addEventListener('click', ()=>{
 
 //dark mode 
 // scroll div
-let sections = document.querySelectorAll('section')
-sections.forEach(section=>{
-    ScrollTrigger.create({
-        trigger:section,
-        start:'top top',
-        pin:true,
-        pinSpacing:false,
-        markers:false,
-        });
-})
+// let sections = document.querySelectorAll('section')
+// sections.forEach(section=>{
+//     ScrollTrigger.create({
+//         trigger:section,
+//         start:'top top',
+//         pin:true,
+//         pinSpacing:false,
+//         markers:false,
+//         });
+// })
 
+gsap.to('#about',{ clipPath:'polygon(0 0, 0 0, 0 0, 0 0)', y:'-100vh',position:'absolute', duration:0});
+gsap.to('#skills',{ clipPath:'polygon(0 0, 0 0, 0 0, 0 0)',y:'-200vh',position:'absolute', duration:0});
+gsap.to('#contact',{ clipPath:'polygon(0 0, 0 0, 0 0, 0 0)',y:'-300vh',position:'absolute', duration:0});
+
+let about = document.querySelector('#about');
+let skills = document.querySelector('#skills');
+let contact = document.querySelector('#contact');
+
+let aboutButton = document.querySelector('#about_button');
+let skillsButton = document.querySelector('#skills_button');
+let contactButton = document.querySelector('#contact_button');
+
+let tl = gsap.timeline(); 
+tl.pause()
+
+function divOnView(button, section,tl){
+    tl.to(button,{ x: 200, duration: 1 },0)
+    tl.to(section,{x:'16vw',duration:1},0)
+    tl.to(section,{clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',duration:1},0)
+}
+
+aboutButton.addEventListener('click',()=>{
+    tl.play()
+    divOnView(aboutButton,about)
+
+})
